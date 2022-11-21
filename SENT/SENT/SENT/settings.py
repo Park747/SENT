11eb2f56e65e6ceaf05fee9d55556fbd41f1661d
yilin-kim <irenek91@korea.ac.kr>
2022-11-22 02:40:22 +0900

    fix: CORS 에러 해결을 위한 settings.py 수정

--- SENT/SENT/SENT/settings.py
+++ SENT/SENT/SENT/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'user',
     'main',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = 'SENT.urls'
